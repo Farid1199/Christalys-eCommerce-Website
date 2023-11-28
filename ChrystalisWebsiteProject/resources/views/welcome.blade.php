d678be85b42ac8b466e9d6c590fcb064aaaad220
Farid Student <220090483@aston.ac.uk>
2023-11-28 15:26:06 +0000

    Wealcome
    Welcome

--- ChrystalisWebsiteProject/resources/views/welcome.blade.php
+++ ChrystalisWebsiteProject/resources/views/welcome.blade.php
@@ -105,7 +105,7 @@ MAIN HEADER
                         @else
                             <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
     
-                            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">About us</a>
+                            <a href="{{ route('aboutus') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">About us</a>
 
                             <a href="{{ route('pruducts') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">pruducts</a>
                         @endauth
