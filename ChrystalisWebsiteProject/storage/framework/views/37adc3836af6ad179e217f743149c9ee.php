90614ab9f4fc6be401051a928beddb57e8ceac69
Farid Student <220090483@aston.ac.uk>
2023-12-02 06:03:02 +0000

    Login Webpage
    Added more css on log in webpage

--- ChrystalisWebsiteProject/storage/framework/views/37adc3836af6ad179e217f743149c9ee.php
+++ ChrystalisWebsiteProject/storage/framework/views/37adc3836af6ad179e217f743149c9ee.php
@@ -1,70 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-  <!-----------------------------
-          TITLE HEAD
--------------------------------->
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Chrystalis</title>
-    <link rel="icon" href="img/favicon.png" type="image/x-icon" />
-    <link
-      rel="stylesheet"
-      href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-    />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-  </head>
+<?php $__env->startSection('title', 'Watch'); ?>
 
-  <body>
-    <!-----------------------------
-        HEADER NAVBAR 
--------------------------------->
 
-    <header>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <div id="logo" style="padding-bottom: 10px">
-            <img
-              src="img/logo.png"
-              alt="TopLeft Logo"
-              style="padding-left: 50px; width: 35%; height: 35%"
-            />
-          </div>
 
-          <form class="d-flex flex-grow-1 mx-auto" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul id="top-nav" class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Products</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Cart</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About Us</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="AccountSelection.html">Log In</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+<?php $__env->startSection('content'); ?>
 
     <!-----------------------------
       LOGIN Section
@@ -81,13 +19,6 @@
       LAST SECTION  
 -------------------------------->
 
-    <footer>
-      <section id="conclusion">
-        <div class="copyright-bottom">
-          <marquee>Copyright © 2023 Polaris. All rights reserved</marquee>
-        </div>
-      </section>
-    </footer>
-  </body>
-</html>
-<?php /**PATH C:\xampp\htdocs\Christalys-eCommerce-Website\ChrystalisWebsiteProject\resources\views/loginUserAdmin.blade.php ENDPATH**/ ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('mainLayout.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Christalys-eCommerce-Website\ChrystalisWebsiteProject\resources\views/loginUserAdmin.blade.php ENDPATH**/ ?>
