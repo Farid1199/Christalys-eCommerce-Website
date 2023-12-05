0536bd68035980fd0013fc17bf148865095e3401
Dhruv Joshi <trijal.dhruv@gmail.com>
2023-12-05 15:21:40 +0000

    Push php

---
+++ ChrystalisWebsiteProject/storage/framework/views/e304aec12923b2be345a9ff1a9f855fd.php
@@ -0,0 +1,121 @@
+<?php $__env->startSection('title', 'Ring'); ?>
+
+
+
+<?php $__env->startSection('content'); ?>
+
+  <main>
+      <!-- Ring 1 -->
+      <section class="product">
+          <img src="ring1.jpg" alt="Ring 1 Image">
+          <div class="product-info">
+              <h2>Sparkling Diamond Ring 1</h2>
+              <p class="price">$999.99</p>
+              <p class="description">This stunning diamond ring is crafted with precision to add a touch of
+                  elegance to your collection.</p>
+              <button onclick="addToCart()">Add to Cart</button>
+          </div>
+      </section>
+ 
+      <!-- Ring 2 -->
+      <section class="product">
+          <img src="ring2.jpg" alt="Ring 2 Image">
+          <div class="product-info">
+              <h2>Exquisite Sapphire Ring</h2>
+              <p class="price">$799.99</p>
+              <p class="description">A beautiful sapphire ring that radiates charm and sophistication.</p>
+              <button onclick="addToCart()">Add to Cart</button>
+          </div>
+      </section>
+
+      <!-- Ring 3 -->
+      <section class="product">
+          <img src="ring3.jpg" alt="Ring 3 Image">
+          <div class="product-info">
+              <h2>Eternal Love Band</h2>
+              <p class="price">$599.99</p>
+              <p class="description">A timeless band symbolizing eternal love and commitment.</p>
+              <button onclick="addToCart()">Add to Cart</button>
+          </div>
+      </section>
+
+      <!-- Ring 3 -->
+      <section class="product">
+          <img src="ring3.jpg" alt="Ring 3 Image">
+          <div class="product-info">
+              <h2>Eternal Love Band</h2>
+              <p class="price">$599.99</p>
+              <p class="description">A timeless band symbolizing eternal love and commitment.</p>
+              <button onclick="addToCart()">Add to Cart</button>
+          </div>
+      </section>
+
+      <!-- Ring 3 -->
+      <section class="product">
+          <img src="ring3.jpg" alt="Ring 3 Image">
+          <div class="product-info">
+              <h2>Eternal Love Band</h2>
+              <p class="price">$599.99</p>
+              <p class="description">A timeless band symbolizing eternal love and commitment.</p>
+              <button onclick="addToCart()">Add to Cart</button>
+          </div>
+      </section>
+
+      <!-- Ring 3 -->
+      <section class="product">
+          <img src="ring3.jpg" alt="Ring 3 Image">
+          <div class="product-info">
+              <h2>Eternal Love Band</h2>
+              <p class="price">$599.99</p>
+              <p class="description">A timeless band symbolizing eternal love and commitment.</p>
+              <button onclick="addToCart()">Add to Cart</button>
+          </div>
+      </section>
+
+      <!-- Ring 3 -->
+      <section class="product">
+          <img src="ring3.jpg" alt="Ring 3 Image">
+          <div class="product-info">
+              <h2>Eternal Love Band</h2>
+              <p class="price">$599.99</p>
+              <p class="description">A timeless band symbolizing eternal love and commitment.</p>
+              <button onclick="addToCart()">Add to Cart</button>
+          </div>
+      </section>
+
+      <!-- Ring 3 -->
+      <section class="product">
+          <img src="ring3.jpg" alt="Ring 3 Image">
+          <div class="product-info">
+              <h2>Eternal Love Band</h2>
+              <p class="price">$599.99</p>
+              <p class="description">A timeless band symbolizing eternal love and commitment.</p>
+              <button onclick="addToCart()">Add to Cart</button>
+          </div>
+      </section>
+
+      <!-- Ring 3 -->
+      <section class="product">
+          <img src="ring3.jpg" alt="Ring 3 Image">
+          <div class="product-info">
+              <h2>Eternal Love Band</h2>
+              <p class="price">$599.99</p>
+              <p class="description">A timeless band symbolizing eternal love and commitment.</p>
+              <button onclick="addToCart()">Add to Cart</button>
+          </div>
+      </section>
+
+
+      <!-- Add more rings following the same structure -->
+
+            
+          
+ 
+    </main>
+
+
+<?php $__env->stopSection(); ?>
+
+
+
+<?php echo $__env->make('mainLayout.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\GitHub\Christalys-eCommerce-Website\ChrystalisWebsiteProject\resources\views/ring.blade.php ENDPATH**/ ?>
