b51b56a7fec3d783c953b1dbf73a9b0109c8f231
Farid Student <220090483@aston.ac.uk>
2023-12-09 02:05:18 +0000

    Minor changes on dashboard.php

--- ChrystalisWebsiteProject/storage/framework/views/3174597db5a47dbeae2134fae3c49adb.php
+++ ChrystalisWebsiteProject/storage/framework/views/3174597db5a47dbeae2134fae3c49adb.php
@@ -6,17 +6,6 @@
 
 
 
-
-<?php if (isset($component)) { $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54 = $component; } ?>
-<?php if (isset($attributes)) { $__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54 = $attributes; } ?>
-<?php $component = App\View\Components\AppLayout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
-<?php $component->withName('app-layout'); ?>
-<?php if ($component->shouldRender()): ?>
-<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
-<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(App\View\Components\AppLayout::class))->getConstructor()): ?>
-<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
-<?php endif; ?>
-<?php $component->withAttributes([]); ?>
      <?php $__env->slot('header', null, []); ?> 
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             <?php echo e(__('Dashboard'), false); ?>
@@ -34,16 +23,132 @@
             </div>
         </div>
     </div>
- <?php echo $__env->renderComponent(); ?>
-<?php endif; ?>
-<?php if (isset($__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
-<?php $attributes = $__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
-<?php unset($__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
-<?php endif; ?>
-<?php if (isset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
-<?php $component = $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
-<?php unset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
-<?php endif; ?>
+
+
+
+
+<link rel="stylesheet" href="<?php echo e(asset('assets/css/css-pages/dashboard.css'), false); ?>" />
+
+<!-----------------------------
+      User Account
+-------------------------------->
+
+<hr class="featurette-divider">
+
+<h1 class="mt-5 text-center" style="padding-top:50px;"> Welcome to your Dashboard, USER! </h1>
+<main class="user-main mt-5">
+
+    <section id="founders-profile">
+        <div class="text-founder1">
+            <ul>
+                <li>
+                    <div class="founder-box4-skills">
+                        <img src="<?php echo e(asset('Images\HomePage\user-pp.png'), false); ?>" alt="java-logo" class="lang-logos" />
+                        <h3>Account Name</h3>
+                        <section class="user-links">
+                            <h4>My Details</h4>
+                        </section>
+                        <section class="user-links">
+                            <h4>Order History</h4>
+                        </section>
+                        <section class="user-links">
+                            <h4>Cart & Wishlist</h4>
+                        </section>
+                        <section class="user-links">
+                            <h4>Account Settings</h4>
+                        </section>
+                        <section class="user-links">
+                            <a href="login.html" class="styleless">
+                                <h4>Log Out</h4>
+                            </a>
+                        </section>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="founder-box4-skills">
+                        <h3 style="padding-top: 10px">Help and Support</h3>
+                        <section class="user-links">
+                            <a href="<?php echo e(route('contactus'), false); ?>" style="text-decoration:none;">
+                                <h4>Customer Support</h4>
+                            </a>
+                        </section>
+                        <section class="user-links">
+                            <a href="<?php echo e(route('contactus'), false); ?>" style="text-decoration:none;">
+                                <h4>Digital Service Support</h4>
+                            </a>
+                        </section>
+                        <section class="user-links">
+                            <a href="<?php echo e(route('contactus'), false); ?>" style="text-decoration:none;">
+                                <h4>Contact Us</h4>
+                            </a>
+                        </section>
+                    </div>
+                </li>
+            </ul>
+
+            <div class="founder-box2">
+                <h3>Account Details</h3>
+
+                <h4>Personal Details</h4>
+                <section class="user-links">
+                    <p style="font-weight: 500">Your Name</p>
+                    <p>DOB: XX/XX/XX</p>
+                    <p>Email: sampleemail@email.com</p>
+                    <p>Phone: +44 XXXX XXX XXX</p>
+                    <p>Current Password: *******</p>
+                    <p style="padding: 15px; color: black">
+                        <a href="#" class="styleless"> Edit Details </a>
+                    </p>
+                </section>
+
+                <h4>Saved Addresses</h4>
+                <section class="user-links">
+                    <p style="font-weight: 500">Your Name</p>
+                    <p>35 London Road, KINGSTON UPON THAMES, KT17 8BM</p>
+                    <p>United Kingdom</p>
+                    <p>Phone: +44 XXXX XXX XXX</p>
+                    <p style="padding: 15px; color: black">
+                        <a href="#" class="styleless"> Edit </a><a href="#" class="styleless"> | Remove </a>
+                    </p>
+                </section>
+
+                <section class="user-links">
+                    <p style="padding-bottom: 5px">
+                        <a href="#" class="styleless"> Add New Address </a>
+                    </p>
+                </section>
+
+                <h4>Saved Payment Details</h4>
+                <section class="user-links">
+                    <p style="font-weight: 500">Card Name: Your Name</p>
+                    <p>Visa Debit ending in XXXX</p>
+                    <p style="padding: 15px; color: black">
+                        <a href="#" class="styleless"> Edit </a><a href="#" class="styleless"> | Remove </a>
+                    </p>
+                </section>
+                <section class="user-links">
+                    <p style="padding-bottom: 5px">
+                        <a href="#" class="styleless"> Add New Payment Option </a>
+                    </p>
+                </section>
+
+                <h4>Gift Cards and Vouchers</h4>
+                <section class="user-links">
+                    <p style="font-weight: 500; padding-bottom: 5px">
+                        Your Balance: £0.00
+                    </p>
+                </section>
+                <section class="user-links">
+                    <p style="padding-bottom: 5px">
+                        <a href="#" class="styleless"> Add New Gift Card/Voucher </a>
+                    </p>
+                </section>
+            </div>
+        </div>
+    </section>
+</main>
+
 
 
 
