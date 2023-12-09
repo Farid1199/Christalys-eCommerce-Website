9997b5e680f6e964c334ac47a45b118e60c738d2
Dhruv Joshi <trijal.dhruv@gmail.com>
2023-12-09 12:59:01 +0000

    log

--- ChrystalisWebsiteProject/app/Http/Controllers/ProductController.php
+++ ChrystalisWebsiteProject/app/Http/Controllers/ProductController.php
@@ -8,8 +8,8 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
 use App\Http\Controllers;
-use Illuminate\Support\Facades\Session; 
-use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
 
 
 
@@ -18,165 +18,169 @@ class ProductController extends Controller
 
 
 
- 
+
 
 
     function detail($id)
     {
-        $data =  Product::find($id);
+        $data = Product::find($id);
         return view('ProductDetail', ['product' => $data]);
-        
+
     }
-  
-
-     // ################################       Ring    #############################################
-     public function index()
-     {
-         //
-         $rings = Product::where('category', 'Ring')->get();
-         return view('ring', ['products' =>$rings]);
-     }
-     public function getRings()
-     {
-         $rings = Product::where('category', 'Ring')->get();
-         return view('ProductDetail.rings', compact('ring'));
-     }
-
-     // ################################       Bracelet    #############################################
-
-     public function index1()
-     {
-         //
-         $bracelets = Product::where('category', 'Bracelet')->get();
-         return view('bracelet', ['products' =>$bracelets]);
-     }
-
-     public function getBracelet()
-     {
-         $bracelets = Product::where('category', 'Bracelet')->get();
-         return view('ProductDetail.bracelets', compact('Bracelet'));
-     }
 
 
+    // ################################       Ring    #############################################
+    public function index()
+    {
+        //
+        $rings = Product::where('category', 'Ring')->get();
+        return view('ring', ['products' => $rings]);
+    }
+    public function getRings()
+    {
+        $rings = Product::where('category', 'Ring')->get();
+        return view('ProductDetail.rings', compact('ring'));
+    }
+
+    // ################################       Bracelet    #############################################
+
+    public function index1()
+    {
+        //
+        $bracelets = Product::where('category', 'Bracelet')->get();
+        return view('bracelet', ['products' => $bracelets]);
+    }
+
+    public function getBracelet()
+    {
+        $bracelets = Product::where('category', 'Bracelet')->get();
+        return view('ProductDetail.bracelets', compact('Bracelet'));
+    }
 
 
-     // ################################       Necklace    #############################################
 
-     public function index2()
-     {
-         //
-         $necklace = Product::where('category', 'Necklace')->get();
-         return view('necklace', ['products' =>$necklace]);
-     }
 
-     public function getNecklace()
-     {
-         $necklace = Product::where('category', 'Necklace')->get();
-         return view('ProductDetail.necklaces', compact('necklace'));
-     }
+    // ################################       Necklace    #############################################
+
+    public function index2()
+    {
+        //
+        $necklace = Product::where('category', 'Necklace')->get();
+        return view('necklace', ['products' => $necklace]);
+    }
+
+    public function getNecklace()
+    {
+        $necklace = Product::where('category', 'Necklace')->get();
+        return view('ProductDetail.necklaces', compact('necklace'));
+    }
 
     // ################################       Earring    #############################################
 
     public function index3()
-     {
-         //
-         $earrings = Product::where('category', 'Earring')->get();
-         return view('earring', ['products' =>$earrings]);
-     }
-
-     public function getEarring()
-     {
-         $earrings = Product::where('category', 'Earring')->get();
-         return view('ProductDetail.earrings', compact('earring'));
-     }
-
-     // ################################       Watch    #############################################
-
-
-     public function index4()
-     {
-         //
-         $watch = Product::where('category', 'Watch')->get();
-         return view('watch', ['products' =>$watch]);
-     }
-
-     public function getWatch()
-     {
-         $watch = Product::where('category', 'Watch')->get();
-         return view('ProductDetail.watches', compact('watch'));
-     }
-
-
-     /**
-      *   ###############    This is a fuction for the cart table  ##################################
-      */
-   
-
-      function addToCart(Request $reg){
-       
-           
-        $cart= new Cart;
-        $cart->user_id = auth()->user()->id;
-        $cart->product_id=$reg->product_id;
-        # then we are saving it
-        $cart->save();
-        return redirect('/');  
-    
-  }
-
-
-  static function cartItem(){
-   // $userId=Session::get('user')['id'];
-    $userId=auth()->id();
-  
-    return Cart::where('user_id',$userId)->count();
-
-  }
- 
-  
-  function cartList(){
-    $userId = auth()->id();
-
-    $products = DB::table('cart')
-        ->join('products', 'cart.product_id', '=', 'products.id')  
-        ->where('cart.user_id', $userId)
-        ->select('products.*', 'cart.id as cart_id')
-        ->get();
-
-    return view('cartlist', ['products' => $products]);
-}
-
-
-####################          Remove from cart fuunction         #######################
-
-public function removeCart($id){
-    // Check if the authenticated user owns the cart item before removing it
-    $cartItem = Cart::find($id);
-
-    if (!$cartItem || $cartItem->user_id !== auth()->id()) {
-        // If the cart item is not found or doesn't belong to the authenticated user
-        abort(403, 'Unauthorized action.');
+    {
+        //
+        $earrings = Product::where('category', 'Earring')->get();
+        return view('earring', ['products' => $earrings]);
     }
 
-    // If the user owns the cart item, proceed to remove it
-    Cart::destroy($id);
+    public function getEarring()
+    {
+        $earrings = Product::where('category', 'Earring')->get();
+        return view('ProductDetail.earrings', compact('earring'));
+    }
 
-    return redirect('cartlist');
-}
-
-
-
-
+    // ################################       Watch    #############################################
 
 
+    public function index4()
+    {
+        //
+        $watch = Product::where('category', 'Watch')->get();
+        return view('watch', ['products' => $watch]);
+    }
 
-     
+    public function getWatch()
+    {
+        $watch = Product::where('category', 'Watch')->get();
+        return view('ProductDetail.watches', compact('watch'));
+    }
 
-    
 
- 
+    /**
+     *   ###############    This is a fuction for the cart table  ##################################
+     */
 
-    
+
+    function addToCart(Request $reg)
+    {
+
+
+        $cart = new Cart;
+        $cart->user_id = auth()->user()->id;
+        $cart->product_id = $reg->product_id;
+        # then we are saving it
+        $cart->save();
+        return redirect('/');
+
+    }
+
+
+    static function cartItem()
+    {
+        // $userId=Session::get('user')['id'];
+        $userId = auth()->id();
+
+        return Cart::where('user_id', $userId)->count();
+
+    }
+
+
+    function cartList()
+    {
+        $userId = auth()->id();
+
+        $products = DB::table('cart')
+            ->join('products', 'cart.product_id', '=', 'products.id')
+            ->where('cart.user_id', $userId)
+            ->select('products.*', 'cart.id as cart_id')
+            ->get();
+
+        return view('cartlist', ['products' => $products]);
+    }
+
+
+    ####################          Remove from cart fuunction         #######################
+
+    public function removeCart($id)
+    {
+        // Check if the authenticated user owns the cart item before removing it
+        $cartItem = Cart::find($id);
+
+        if (!$cartItem || $cartItem->user_id !== auth()->id()) {
+            // If the cart item is not found or doesn't belong to the authenticated user
+            abort(403, 'Unauthorized action.');
+        }
+
+        // If the user owns the cart item, proceed to remove it
+        Cart::destroy($id);
+
+        return redirect('cartlist');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     /**
--- ChrystalisWebsiteProject/resources/views/cartlist.blade.php
+++ ChrystalisWebsiteProject/resources/views/cartlist.blade.php
@@ -7,108 +7,110 @@
 @section('content')
 
 <div class="container py-5">
-    <div class="py-5 text-center"></div>
+  <div class="py-5 text-center"></div>
 
-    
-  
-    <div class="row">
-      <div class="col-md-4 order-md-2 mb-4">
-        <h4 class="d-flex justify-content-between align-items-center mb-3">
-          <span class="text-muted">Your cart</span>
-          <span class="badge badge-secondary badge-pill">10</span>
-        </h4>
-        <ul class="list-group mb-3">
 
-            @foreach($products as $item)
 
-          <li
-            class="list-group-item d-flex justify-content-between lh-condensed"
-          >
+  <div class="row">
+    <div class="col-md-4 order-md-2 mb-4">
+      <h4 class="d-flex justify-content-between align-items-center mb-3">
+        <span class="text-muted">Your cart</span>
+        <span class="badge badge-secondary badge-pill">10</span>
+      </h4>
+      <ul class="list-group mb-3">
 
-          
-            <div>
-              <a href="detail/{{$item->id}}" style="text-decoration:none;"> <h6 class="my-0">{{$item->name}}</h6></a>
-              <small class="text-muted">{{$item->description}}</small>
-            </div>
-            <span class="text-muted">{{$item->price}}</span>
-          </li>
+        @foreach($products as $item)
 
-          @endforeach
+        <li class="list-group-item d-flex justify-content-between lh-condensed">
 
-          <li class="list-group-item d-flex justify-content-between">
-            <span>Total (USD)</span>
-            <strong>{{$item->price}}</strong>
-          </li>
 
-          
-        </ul>
-  
-        <div>     
-                
-            <a href="check-out-page2.html"><button type="button" class="btn btn-primary btn-block btn-lg">Proceed to Checkout</button></a>
-            </div>
-  
+          <div>
+            <a href="detail/{{$item->id}}" style="text-decoration:none;">
+              <h6 class="my-0">{{$item->name}}</h6>
+            </a>
+            <small class="text-muted">{{$item->description}}</small>
+          </div>
+          <span class="text-muted">{{$item->price}}</span>
+        </li>
+
+        @endforeach
+
+        <li class="list-group-item d-flex justify-content-between">
+          <span>Total (USD)</span>
+          <strong>{{$item->price}}</strong>
+        </li>
+
+
+      </ul>
+
+      <div>
+
+        <a href="check-out-page2.html"><button type="button" class="btn btn-primary btn-block btn-lg">Proceed to
+            Checkout</button></a>
       </div>
 
-      
+    </div>
 
 
-      <div class="col-md-8 order-md-1">
-        <h4 class="mb-3">Shopping Cart</h4>
-       
-          <div class="row">
-            @foreach($products as $item)
-            <div class="card rounded-3 mb-4">
-                
-              <div class="card-body">
-                <div class="row d-flex justify-content-between align-items-center">
-                  <div class="col-md-2 col-lg-2 col-xl-2">
 
-                    
 
-                    <img
-                      src="{{$item->gallery}}"
-                      class="img-fluid rounded-3" alt="Cotton T-shirt">
-                  </div>
-                  <div class="col-md-3 col-lg-3 col-xl-3">
-                    <a href="detail/{{$item->id}}" style="text-decoration:none;"><p class="lead fw-normal mb-2">{{$item->name}}</p></a>
-                  </div>
-                  <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                    <h5 class="mb-0">{{$item->price}}</h5>
-                  </div>
-                  <div class="col-md-1 col-lg-1 col-xl-1 text-end">               
-                    <a href="{{ url('/removecart', ['id' => $item->id]) }}">
-                      <img src="{{ asset('Images\HomePage\cart-remove.png') }}" class="img-fluid">
-                  </a>
-                                    </div>
-                  
-                </div>
+    <div class="col-md-8 order-md-1">
+      <h4 class="mb-3">Shopping Cart</h4>
+
+      <div class="row">
+        @foreach($products as $item)
+        <div class="card rounded-3 mb-4">
+
+          <div class="card-body">
+            <div class="row d-flex justify-content-between align-items-center">
+              <div class="col-md-2 col-lg-2 col-xl-2">
+
+
+
+                <img src="{{$item->gallery}}" class="img-fluid rounded-3" alt="Cotton T-shirt">
               </div>
-              
+              <div class="col-md-3 col-lg-3 col-xl-3">
+                <a href="detail/{{$item->id}}" style="text-decoration:none;">
+                  <p class="lead fw-normal mb-2">{{$item->name}}</p>
+                </a>
+              </div>
+              <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                <h5 class="mb-0">{{$item->price}}</h5>
+              </div>
+              <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                <a href="{{ url('/removecart', ['id' => $item->id]) }}">
+                  <img src="{{ asset('Images\HomePage\cart-remove.png') }}" class="img-fluid">
+                </a>
+              </div>
+
             </div>
-            @endforeach
           </div>
 
-
-          
-   
-            <div class="row">          
-                
-                  <a href="check-out-page2.html"><button type="button" class="btn btn-primary btn-block btn-lg">Proceed to Checkout</button></a>
-                
-            </div>
-  
-  
+        </div>
+        @endforeach
       </div>
-  
-      
 
-  
+
+
+
+      <div class="row">
+
+        <a href="check-out-page2.html"><button type="button" class="btn btn-primary btn-block btn-lg">Proceed to
+            Checkout</button></a>
+
+      </div>
+
+
     </div>
-  
+
+
 
 
   </div>
+
+
+
+</div>
 
 
 
