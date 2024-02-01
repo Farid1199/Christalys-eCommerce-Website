8a730d17a4dbd23d9ae2927bdbfdf544e80abfb9
Dhruv Joshi <trijal.dhruv@gmail.com>
2024-02-01 00:38:07 +0000

    Improvement on header and footer
    Made footer more professional, added social media links, and more useful links to the footer. Next aim is to make header more stylised and user friendly.

--- ChrystalisWebsiteProject/resources/views/mainLayout/layout.blade.php
+++ ChrystalisWebsiteProject/resources/views/mainLayout/layout.blade.php
@@ -28,6 +28,10 @@ $total = ProductController::cartItem();
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
   <link rel="stylesheet" href="{{ asset('assets/css/css-pages/ministore.css')}}" />
 
+
+  <!-- Bootstrap icons -->
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
   <!--
   -->
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
@@ -148,23 +152,133 @@ $total = ProductController::cartItem();
 
 
   @yield('footer')
-  <footer>
-    <section id="conclusion">
-      <div class="copyright-bottom text-center">
-        <p class="m-1">
-          &copy; Copyright Chrystalis 2023-2024. All Rights Reserved
-        </p>
-        <ul class="list-inline">
-          <li class="list-inline-item"><a href="{{ asset('Images\HomePage\privacyp.png') }}">Privacy</a></li>
-          <li class="list-inline-item"><a href="{{ asset('Images\HomePage\privacyp.png') }}">Terms</a></li>
-          <li class="list-inline-item"><a href="{{route('contactus')}}">Support</a></li>
-          <li class="list-inline-item"><a href="#">Back to top</a></li>
-        </ul>
+  <!-- Footer -->
+  <footer class="text-center text-lg-start text-dark" style="background-color: #eceff1">
+
+
+    <!-- Section: Social media -->
+    <section id="conclusion" class="d-flex justify-content-between p-4 text-white" style="background-color: #969696">
+
+      <!-- Left -->
+      <div class="me-5">
+        <span>Get connected with us on social networks:</span>
+      </div>
+      <!-- Left -->
+
+      <!-- Right -->
+      <div>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="text-md-start mt-5 px-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md col-lg col-xl mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold">Chrystalis</h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+            <p>
+              Thank you for choosing Chrystalis. We look forward to being a
+              part of your special moments and helping you express your unique
+              style and individuality through our stunning jewelry collection.
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md col-lg col-xl-3 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Information</h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+            <p>
+              <a href="{{route('welcome')}}" class="list-inline-item">Home page</a>
+            </p>
+            <p>
+              <a href="{{route('products')}}" class="list-inline-item">Browse Our Catalogue</a>
+            </p>
+            <p>
+              <a href="{{route('contactus')}}" class="list-inline-item">Contact Us</a>
+            </p>
+            <p>
+              <a href="{{route('aboutus')}}" class="list-inline-item">About Us</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md col-lg col-xl-3 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Useful links</h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+            <p>
+              <a href="{{route('dashboard')}}" class="list-inline-item">Your Account</a>
+            </p>
+            <p>
+              <a href="{{route('cartlist')}}" class="list-inline-item">Your Cart</a>
+            </p>
+            <p>
+              <a href="{{route('previousod')}}" class="list-inline-item">Previous Orders</a>
+            </p>
+            <p>
+              <a href="{{ route('profile.edit') }}" class="list-inline-item">Account Settings</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md col-lg col-xl mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Contact Us</h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+            <p>
+              <i class="fas fa-home mr-1"></i> 33 Road, Birmingham, B87 8HG
+            </p>
+            <p><i class="fas fa-envelope mr-1"></i> chrystalis@example.com</p>
+            <p><i class="fas fa-phone mr-1"></i> + 44 2345 678 098</p>
+            <p><i class="fas fa-print mr-1"></i> + 44 2342 567 389</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
       </div>
     </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05); text-decoration: none">
+      © Copyright Chrystalis 2023-2024. All Rights Reserved
+
+      <ul class="list-inline pt-2">
+        <li class="list-inline-item"><a href="{{ asset('Images\HomePage\privacyp.png') }}">Privacy</a></li>
+        <li class="list-inline-item"><a href="{{ asset('Images\HomePage\privacyp.png') }}">Terms</a></li>
+        <li class="list-inline-item"><a href="{{route('contactus')}}">Support</a></li>
+        <li class="list-inline-item"><a href="#">Back to top</a></li>
+      </ul>
+    </div>
+
+
+    </section>
   </footer>
-
-
 
 </body>
 
