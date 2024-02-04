772539d9f69b2281a5f701be9cccc5f01f9b8504
Dhruv Joshi <trijal.dhruv@gmail.com>
2024-02-04 02:38:49 +0000

    minor changes on dashboard

--- ChrystalisWebsiteProject/resources/views/dashboard.blade.php
+++ ChrystalisWebsiteProject/resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 @extends('mainLayout.layout')
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -37,6 +38,7 @@
 
 
     <div>
+        <hr class="mb-3">
         <h2 class="text-center my-3 py-3"> Welcome to your Dashboard, {{ Auth::user()->name }}!
         </h2>
     </div>
