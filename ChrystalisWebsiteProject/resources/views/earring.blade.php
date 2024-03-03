1977453de9e28275076c61c7c47c4ffa56f3c2df
Dhruv Joshi <trijal.dhruv@gmail.com>
2024-03-03 02:10:13 +0000

    update earring blade for minor fix

--- ChrystalisWebsiteProject/resources/views/earring.blade.php
+++ ChrystalisWebsiteProject/resources/views/earring.blade.php
@@ -16,91 +16,93 @@
     <div class="container py-5">
 
         <div class="py-3 text-center"></div>
-        <div class="row">
+            <div class="row">
 
-        <!-- Search Filters Column -->
-        <div class="col-md-3">
-                <div class="card sticky-sm-top  mt-4">
-                    <div class="card-body ">
+                <!-- Search Filters Column -->
+                <div class="col-md-3">
+                    <div class="card sticky-sm-top  mt-4">
+                        <div class="card-body ">
 
 
 
-                    <h5 class="card-title">Search Filters</h5>
-                    <!-- Search form for category -->
-                    <form>
+                            <h5 class="card-title">Search Filters</h5>
+                            <!-- Search form for category -->
+                            <form>
 
-                        <div class="form-group">
-                            <label for="min_price">Search by Price</label>
-                            <input type="number" class="form-control" id="min_price" name="min_price"
-                                placeholder="Min Price" value="{{ request('min_price') }}">
-                            <input type="number" class="form-control" id="max_price" name="max_price"
-                                placeholder="Max Price" value="{{ request('max_price') }}">
+                                <div class="form-group">
+                                    <label for="min_price">Search by Price</label>
+                                    <input type="number" class="form-control" id="min_price" name="min_price"
+                                        placeholder="Min Price" value="{{ request('min_price') }}">
+                                    <input type="number" class="form-control" id="max_price" name="max_price"
+                                        placeholder="Max Price" value="{{ request('max_price') }}">
+                                </div>
+
+                                <div class="form-group mt-5 text-center">
+                                    <button class="btn btn-success text-center" type="submit" >Apply Filters</button>
+                                    <button class="btn btn-primary ml-3" value="{{ request('reset_filters') }} ">Reset Filters</button>
+                                </div>
+                        
+                            </form>
                         </div>
-
-                        <div class="form-group mt-5 text-center">
-                            <button class="btn btn-success text-center" type="submit" >Apply Filters</button>
-                            <button class="btn btn-primary ml-3" value="{{ request('reset_filters') }} ">Reset Filters</button>
-                        </div>
-                
-                    </form>
                     </div>
                 </div>
-            </div>
+                
 
-            <div class="col-md-9">
+                <div class="col-md-9">
 
-                @foreach ($products as $earring)
+                    @foreach ($products as $earring)
 
-                <div class="card mb-4 box-shadow item{{ $earring['id'] == 1 ? 'active' : '' }}">
-                    <div class="row align-items-center">
-                        <div class="col text-center">
-                            <img class="card-img-center img-fluid img-responsive" src="{{ $earring['gallery'] }}"
-                                style="width: 250px; height: 250px;" alt="Card image cap" />
-                        </div>
-                        <div class="col-8">
-                            <div class="card-body text-left">
-                                <h4 class="text-left my-3">{{ $earring->name }}</h4>
-                                <p class="card-text">
-                                    {{ $earring->description }}
-                                </p>
+                    <div class="card mb-4 box-shadow item{{ $earring['id'] == 1 ? 'active' : '' }}">
+                        <div class="row align-items-center">
+                            <div class="col text-center">
+                                <img class="card-img-center img-fluid img-responsive" src="{{ $earring['gallery'] }}"
+                                    style="width: 250px; height: 250px;" alt="Card image cap" />
+                            </div>
+                            <div class="col-8">
+                                <div class="card-body text-left">
+                                    <h4 class="text-left my-3">{{ $earring->name }}</h4>
+                                    <p class="card-text">
+                                        {{ $earring->description }}
+                                    </p>
 
-                                <p class="card-text font-weight-bold">
-                                    Price: £{{ $earring->price}}
-                                </p>
+                                    <p class="card-text font-weight-bold">
+                                        Price: £{{ $earring->price}}
+                                    </p>
 
-                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="d-flex justify-content-between align-items-center">
 
-                                    <form action="detail/{{$earring['id']}}" method="GET">
-                                        @csrf
-                                        <button class="btn btn-outline-primary" id="addToCartBtn"> View </button>
-                                    </form>
+                                        <form action="detail/{{$earring['id']}}" method="GET">
+                                            @csrf
+                                            <button class="btn btn-outline-primary" id="addToCartBtn"> View </button>
+                                        </form>
 
-                                    <form action="/add_to_wishlist" method="POST">
-                                        @csrf
-                                        <button class="btn btn-outline-secondary" id="addToCartBtn"> Add to Wishlist
-                                        </button>
-                                        <input type="hidden" value="{{$earring->id}}" name="product_id" id="product_id">
-                                    </form>
+                                        <form action="/add_to_wishlist" method="POST">
+                                            @csrf
+                                            <button class="btn btn-outline-secondary" id="addToCartBtn"> Add to Wishlist
+                                            </button>
+                                            <input type="hidden" value="{{$earring->id}}" name="product_id" id="product_id">
+                                        </form>
 
-                                    <form action="/add_to_cart" method="POST">
-                                        @csrf
-                                        <input type="hidden" name="product_id" value="{{$earring['id']}}">
-                                        <button class="btn btn-success" id="addToCartBtn"> Add to Cart </button>
-                                    </form>
+                                        <form action="/add_to_cart" method="POST">
+                                            @csrf
+                                            <input type="hidden" name="product_id" value="{{$earring['id']}}">
+                                            <button class="btn btn-success" id="addToCartBtn"> Add to Cart </button>
+                                        </form>
 
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
+                    @endforeach
+
                 </div>
 
-                @endforeach
-
-            </div>
         </div>
     </div>
 </div>
-</div>
+
 
 
 <!--
