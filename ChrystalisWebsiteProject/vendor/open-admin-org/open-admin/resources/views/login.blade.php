c87ad39ddbf278ee70f31b0b262bf13f022e83fa
Farid Student <220090483@aston.ac.uk>
2024-03-20 08:44:36 +0000

    Admin Page

--- ChrystalisWebsiteProject/vendor/open-admin-org/open-admin/resources/views/login.blade.php
+++ ChrystalisWebsiteProject/vendor/open-admin-org/open-admin/resources/views/login.blade.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>{{config('admin.title')}} | {{ __('admin.login') }}</title>
+		<title>Chrystalis | {{ __('admin.login') }}</title>
 		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
 		@if(!is_null($favicon = Admin::favicon()))
@@ -17,7 +17,7 @@
 	<body class="bg-light" @if(config('admin.login_background_image'))style="background: url({{config('admin.login_background_image')}}) no-repeat;background-size: cover;"@endif>
 		<div class="d-flex justify-content-center align-items-center h-100">
 			<div class="container m-4" style="max-width:400px;">
-				<h1 class="text-center mb-3 h2"><a class="text-decoration-none text-dark" href="{{ admin_url('/') }}">{{config('admin.name')}}</a></h1>
+				<h1 class="text-center mb-3 h2"><a class="text-decoration-none text-dark" href="{{ admin_url('/') }}">Chrystalis</a></h1>
 				<div class="bg-body p-4 shadow-sm rounded-3">
 
 					@if($errors->has('attempts'))
